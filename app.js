d23c3893182000f5484dc51e9ee684b1fecf94a7
Vladimir Vuksanovic <vladevuksan99@gmail.com>
2019-03-15 10:42:01 +0100

    Update back end dependencies

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(function(req, res, next) {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use(passport.initialize());
 app.use(passport.session());
